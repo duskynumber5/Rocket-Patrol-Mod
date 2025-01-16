8ef44c705380d1753b30755875617305a26dc707
Maddison Lobo <mplobo@ucsc.edu>
2025-01-16 13:54:11 -0800

    rockets flying

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -17,6 +17,11 @@ class Play extends Phaser.Scene {
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0)
 
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0)
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0)
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0)
+
 
         // define keys
         keyFIRE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)
@@ -30,5 +35,9 @@ class Play extends Phaser.Scene {
         this.starfield.tilePositionX -= 4
         // update rocket w/ player input
         this.p1Rocket.update() 
+        // update spaceships (x3)
+        this.ship01.update()
+        this.ship02.update()
+        this.ship03.update()
     }
 }

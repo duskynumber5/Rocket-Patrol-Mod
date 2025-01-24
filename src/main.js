b5b047e75b001d485c3288e5d51d344a16b803f9
Maddison Lobo <mplobo@ucsc.edu>
2025-01-24 09:57:39 -0800

    initial w/ main comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+// Maddison Lobo
+// Rocket Patrol 2: Double Trouble
+// _ hours
+
+// the mods you chose, their point values, and if necessary, an explanation of their implementation
+    // Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+    // Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+    // Create a new title screen (e.g., new artwork, typography, layout) (3)
+    // Display the time remaining (in seconds) on the screen (3)
+    // Implement parallax scrolling for the background (3)
+    // Allow the player to control the Rocket after it's fired (1)
+    // Implement the speed increase that happens after 30 seconds in the original game (1)
+    // Track a high score that persists across scenes and display it in the UI (1)
+    // Implement the 'FIRE' UI text from the original game (1)
+
+// citations
+    // 
+
 // make screen size & add scenes
 let config = {
     type: Phaser.AUTO,
